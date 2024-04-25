df9380b87eb93f67094079281b876c751418d1b9
Yagunio <nikitavitlev@mail.ru>
2024-04-25 20:03:11 +0700

    тест

--- src/Third.java
+++ src/Third.java
@@ -5,11 +5,11 @@ public class Third {
     */
 
     public static void main(String[] args) {
-        out_for:
+        out:
         for (int i = 2; i < 101; i++) {
             for (int j = 2; j < i; j++) {
                 if (i%j == 0) {
-                    continue out_for;
+                    continue out;
                 }
             }
             System.out.println(i);
